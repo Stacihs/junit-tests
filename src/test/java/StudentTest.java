036e523fd52819f99198a323ec7f8e3fd60b1e9c
Staci Holmes-Segarra <staci.holmessegarra@gmail.com>
2023-11-21 13:39:57 -0600

    Added fields and methods to Student class

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,2 +1,36 @@
+import lombok.Getter;
+
+import java.util.ArrayList;
+
 public class StudentTest {
+    // returns the student's name
+    @Getter
+    private String name;
+    // returns the student's id
+    @Getter
+    private long id;
+    @Getter
+    private ArrayList<Integer> grades;
+
+    public StudentTest(String name, long id, ArrayList<Integer> grades) {
+        this.name = name;
+        this.id = id;
+        this.grades = grades;
+    }
+
+    // adds the given grade to the grades list
+    public void addGrade(int grade){
+        grades.add(grade);
+    }
+
+    // returns the average of the students grades
+    public double getGradeAverage(){
+        double sum = 0;
+        for (int grade : grades) {
+            sum += grade;
+        }
+        return sum / grades.size();
+    }
+
+
 }
