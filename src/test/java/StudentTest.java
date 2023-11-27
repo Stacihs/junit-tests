1dc0b19a57702ce2ec73e55f7742732907ce2c59
Staci Holmes-Segarra <staci.holmessegarra@gmail.com>
2023-11-27 00:32:49 -0600

    Added tests for Student class

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,36 +1,56 @@
-import lombok.Getter;
+import org.junit.Test;
 
 import java.util.ArrayList;
 
+import static org.junit.Assert.*;
+
+
 public class StudentTest {
-    // returns the student's name
-    @Getter
-    private String name;
-    // returns the student's id
-    @Getter
-    private long id;
-    @Getter
-    private ArrayList<Integer> grades;
-
-    public StudentTest(String name, long id, ArrayList<Integer> grades) {
-        this.name = name;
-        this.id = id;
-        this.grades = grades;
+    @Test
+    public void testIDLong() {
+        Student robin = new Student("Robin", 23890, new ArrayList<>(98));
+        long expected = 23890;
+        long actual = 23890;
+        assertEquals(expected, actual);
     }
 
-    // adds the given grade to the grades list
-    public void addGrade(int grade){
-        grades.add(grade);
+    @Test
+    public void testNameIsString() {
+        Student robin = new Student("Robin", 23890, new ArrayList<Integer>(100));
+        String expected = "Robin";
+        String actual = "Robin";
+        assertEquals(expected, actual);
     }
 
-    // returns the average of the students grades
-    public double getGradeAverage(){
-        double sum = 0;
-        for (int grade : grades) {
-            sum += grade;
-        }
-        return sum / grades.size();
+    @Test
+    public void testGradeAvg() {
+        Student robin = new Student("Robin", 23890, new ArrayList<>());
+        robin.addGrade(80);
+        robin.addGrade(90);
+        robin.addGrade(100);
+        int expected = 90;
+        int actual = 90;
+        assertEquals(expected, actual);
     }
 
+    @Test
+    public void testAddGrade() {
+        Student robin = new Student("Robin", 23890, new ArrayList<>());
+        robin.addGrade(95);
+        int expected = 1;
+        int actual = robin.getGrades().size();
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void testGetGrades() {
+        Student robin = new Student("Robin", 23890, new ArrayList<>());
+        robin.addGrade(70);
+        robin.addGrade(95);
+        int expected = robin.getGrades().get(0);
+        int actual = 70;
+        assertEquals(expected, actual);
+    }
 
 }
